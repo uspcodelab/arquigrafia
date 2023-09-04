5ab84654e4a9f1ce72ba3dad93d16441c8f3ce33
duvanel <thiago.duvanel@usp.br>
2023-09-04 16:20:22 -0300

    Mapa adicionado

--- src/App.tsx
+++ src/App.tsx
@@ -4,33 +4,8 @@ import { MapContainer, TileLayer,  Marker, Popup} from 'react-leaflet'
 import './App.css'
 import 'leaflet/dist/leaflet.css';
 
-interface MarkerData {
-    imageUrl : string;
-    description: string;
-    author: string;
-    position: Array<number>
-}
 
-function ArqMarker (prop: MarkerData) {
-
-    return (
-        <Marker position={prop.position}>
-            <Popup>
-                <img src={prop.imageUrl}></img>
-                {prop.description}
-                <br/><br/>
-                Autor: {prop.author}
-            </Popup>
-        </Marker>
-    )
-}
 function App() {
-    const markers : React.ReactElement[] = [
-        <ArqMarker key="marker1" imageUrl="https://www.tenhomaisdiscosqueamigos.com/wp-content/uploads/2018/01/bryan-cranston-breaking-bad.jpg" position={[51.505, -0.09]} description="ablueblabelabel" author="kutukalku"/>
-    ];
-    const [markersComponents, setMarker] = useState<React.ReactElement[]>(markers);
-    
-
     const tileLayerProps = { 
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
         url:"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -44,7 +19,6 @@ function App() {
         <div>
             <MapContainer {...mapContainerProps}>  
             <TileLayer {...tileLayerProps}/>
-            {markersComponents}
             </MapContainer>
         </div>
     )
