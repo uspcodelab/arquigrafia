9fbbfc8be7516be1f8e3ce94886de7de06921a58
ftressmann <filipetressmann2@gmail.com>
2023-09-04 11:41:32 -0300

    marcadores customizados adicionados

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
 import React from 'react'
-import { useState } from 'react';
-import { MapContainer, TileLayer,  Marker, Popup} from 'react-leaflet'
+import { useState, useRef } from 'react';
+import { MapContainer, TileLayer,  Marker, Popup, useMapEvents} from 'react-leaflet';
+import L, { LatLngExpression } from "leaflet";
 import './App.css'
 import 'leaflet/dist/leaflet.css';
 import markersJson from './markers.json';
 
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+const DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 interface MarkerData {
     imageUrl : string;
     description: string;
     author: string;
-    position: Array<number>
+    position: number[];
 }
 
 function ArqMarker (prop: MarkerData) {
+
+    const pos : LatLngExpression = [prop.position[0], prop.position[1]];
+
     return (
-        <Marker position={prop.position}>
-            <Popup>
+        <Marker position={pos}>
+            <Popup className="arq-popup">
                 <img src={prop.imageUrl} className="popup-image"/>
-                {prop.description}
-                <br/><br/>
-                Autor: {prop.author}
+                <p>{prop.description}</p>               
+                <p>Autor: {prop.author}</p>
             </Popup>
         </Marker>
     )
@@ -37,26 +49,65 @@ function getMarkerComponents() : React.ReactElement [] {
     return markerComponents
 }
 
+function CustomMarker({OnCreateMarker} : any) {
+    const descriptionRef = useRef(null);
+    const authorRef = useRef(null);
+    const [position, setPosition] = useState<LatLngExpression>()
+    const map = useMapEvents({
+      click(e : any) {
+         const newPos : LatLngExpression = [e.latlng.lat, e.latlng.lng];
+         setPosition(newPos);
+      }
+    })
+
+    function OnClick(e : any) {
+        e.stopPropagation();
+        if (authorRef.current == null || descriptionRef.current == null) {
+            return;
+        }
+
+        OnCreateMarker(authorRef.current['value'], descriptionRef.current['value'], position);
+        setPosition(undefined);
+    }
+  
+    return position === undefined ? null : (
+      <Marker position={position}>
+        <Popup>
+            Adicione a descrição:
+          <textarea ref={descriptionRef} defaultValue={"Você está aqui!"} rows={4} cols={40}/>
+          Autor:
+          <textarea ref={authorRef} defaultValue={""} rows={1} cols={40}/>
+          <button onClick={(e) => {OnClick(e)}}> Adicionar marcador</button>
+          
+        </Popup>
+      </Marker>
+    )
+}
+
 function App() {
-    const markerComponents : React.ReactElement[] = getMarkerComponents()    
 
-    const [markersComponents, setMarker] = useState<React.ReactElement[]>(markerComponents);
+    const [markersComponents, setMarkers] = useState<React.ReactElement[]>(getMarkerComponents());
     
-
     const tileLayerProps = { 
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
         url:"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     }
 
-    const mapContainerProps = {
-        center: [-23.55993522722115, -46.72985308377932],
-        zoom: 13
+    function AddMarker(auth : string, desc : string, pos : number[]) {
+        console.log(desc);
+        console.log(auth);
+        const newMarker = markersComponents.slice();
+        newMarker.push(<ArqMarker imageUrl={"https://static.wikia.nocookie.net/breakingbad/images/4/46/BCS_S6_Portrait_Mike.jpg/revision/latest?cb=20220522174959"} position={pos} author={auth} description={desc}/>)
+        setMarkers(newMarker);
     }
+
     return (
         <div>
-            <MapContainer {...mapContainerProps}>  
+            <MapContainer center={[-23.55993522722115, -46.72985308377932]} 
+                          zoom={13}>  
             <TileLayer {...tileLayerProps}/>
-            {markersComponents}
+                <div>{markersComponents}</div>
+                <CustomMarker OnCreateMarker={AddMarker}/>
             </MapContainer>
         </div>
     )
